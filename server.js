5cbb0f1de618bb2e2d10a09147d64df3074e66fe
amnw12 <48080226+amnw12@users.noreply.github.com>
2022-05-19 01:50:47 +1200

    make it better

--- server.js
+++ server.js
@@ -16,11 +16,21 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/submitted', submitted);
 
 const port = process.env.PORT || 8082;
+
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./front-end/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./front-end//build", "index.html"));
+});
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
