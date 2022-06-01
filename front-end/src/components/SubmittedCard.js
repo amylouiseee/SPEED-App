c853357bec5163b01aea72ef38db1ffad82ee845
amylouiseee <ameh456@gmail.com>
2022-06-01 22:02:10 +1200

    added onclick to buttons

--- front-end/src/components/SubmittedCard.js
+++ front-end/src/components/SubmittedCard.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../App.css';
 
 const SubmittedCard = (props) => {
     const  article  = props.article;
+
+    const allow = e => { //pass through to the articles to analyse
+        e.preventDefault();
+        console.log("allow");
+    };
+
+    const deny = e => { //delete article from submitted
+        e.preventDefault();
+        console.log("deny");
+    };
+
+    const check = e => { //this is where you query the db to check if article is duplicate
+        e.preventDefault();
+        console.log("check");
+    }
 
     return(
         <div className="card-container">
@@ -11,8 +25,9 @@ const SubmittedCard = (props) => {
                 <h2>{article.title}</h2>
                 <h3>{article.authors}</h3>
                 <p>{article.source}</p>
-                <button>Allow</button>
-                <button>Deny</button>
+                <button onClick={allow}>Allow</button>
+                <button onClick ={deny}>Deny</button>
+                <button onClick ={check}>Check</button>
             </div>
         </div>
     )
