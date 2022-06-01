1dc852d2aa9ef617fadf524853a3fcbec05a179a
amylouiseee <ameh456@gmail.com>
2022-06-01 21:02:14 +1200

    added analyse card component

--- front-end/src/components/AnalyseCard.js
+++ front-end/src/components/AnalyseCard.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import '../App.css';
+
+const AnalyseCard = (props) => {
+    const article = props.article;
+
+    return (
+        <div className ="card-container">
+            <div className="desc">
+                <h2>{article.title}</h2>
+                <h3>{article.authors}</h3>
+                <p>{article.source}</p>
+            </div>
+        </div>
+    );
+};
+
+export default AnalyseCard;
